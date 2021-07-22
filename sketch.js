852c8bd6b6670a6df3fa3c8b713d8ec6d136b49c
Biffy21 <87482430+Biffy21@users.noreply.github.com>
2021-07-22 12:15:20 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,19 +2,27 @@ var box;
 function setup() {
   createCanvas(400,400);
 box= createSprite(200,200,30,30);
+box.shapeColor=("black");
 }
 function draw() 
 {
   background(30);
   if(keyIsDown(RIGHT_ARROW)){
     box.position.x=box.position.x+5;
+    background("red");
   }
   if(keyIsDown(LEFT_ARROW)){
     box.position.x=box.position.x-5;
+    background("yellow");
   }
   if(keyIsDown(DOWN_ARROW)){
     background("blue");
+    box.position.y=box.position.y+5;
   }
+ if(keyIsDown(UP_ARROW)){
+  background("green");
+  box.position.y=box.position.y-5;
+ }
 
 drawSprites();
 }
